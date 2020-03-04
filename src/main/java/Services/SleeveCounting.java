d157141b515a5bc34dcd15ff40c523d538a48c1c
User <a.popovych89@gmail.com>
2020-03-04 14:17:49 +0200

    New

--- src/main/java/Services/SleeveCounting.java
+++ src/main/java/Services/SleeveCounting.java
@@ -9,13 +9,13 @@ public class SleeveCounting {
             String[] subStr;
             subStr = designation[i].split(delimeter);
             for (int j = 0; j < subStr.length; j++) {
-                if (sleeveSize[0].equals(subStr[j])) {
+                if (sleeveSize[0].equals(subStr[j]) | (" " + sleeveSize[0] + " ").equals(subStr[j])) {
                     sleeveQuantity[0] = sleeveQuantity[0] + quantity[i];
-                } else if (sleeveSize[1].equals(subStr[j])) {
+                } else if (sleeveSize[1].equals(subStr[j]) | (" " + sleeveSize[1] + " ").equals(subStr[j]) | (" " + sleeveSize[1] + "").equals(subStr[j]) | ("" + sleeveSize[1] + " ").equals(subStr[j])) {
                     sleeveQuantity[1] = sleeveQuantity[1] + quantity[i];
-                } else if (sleeveSize[2].equals(subStr[j])) {
+                } else if (sleeveSize[2].equals(subStr[j]) | (" " + sleeveSize[2] + " ").equals(subStr[j]) | (" " + sleeveSize[2] + "").equals(subStr[j]) | ("" + sleeveSize[2] + " ").equals(subStr[j])) {
                     sleeveQuantity[2] = sleeveQuantity[2] + quantity[i];
-                } else if (sleeveSize[3].equals(subStr[j])) {
+                } else if (sleeveSize[3].equals(subStr[j]) | (" " + sleeveSize[3] + " ").equals(subStr[j]) | (" " + sleeveSize[3] + "").equals(subStr[j]) | ("" + sleeveSize[3] + " ").equals(subStr[j])) {
                     sleeveQuantity[3] = sleeveQuantity[3] + quantity[i];
                 }
             }

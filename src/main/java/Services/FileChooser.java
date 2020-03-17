01cf82aca5ee21ddff53276e8d352a54b3efd50f
User <a.popovych89@gmail.com>
2020-03-17 17:21:42 +0200

    New

--- src/main/java/Services/FileChooser.java
+++ src/main/java/Services/FileChooser.java
@@ -1,19 +1,25 @@
 package Services;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileChooser {
 
-    private final String[][] FILTERS = {{"docx", "Файлы Word (*.docx)"},
-            {"pdf" , "Adobe Reader(*.pdf)"}};
-
     public static String fileChoose() {
         JButton open = new JButton();
+
         JFileChooser fc = new JFileChooser();
         fc.setCurrentDirectory(new java.io.File("D:\\"));
         fc.setDialogTitle("Знайдіть файл специфікації !!!");
         fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        if (fc.showOpenDialog(open) == JFileChooser.APPROVE_OPTION) {
+
+
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "Excel", "xls");
+        fc.setFileFilter(filter);
+
+
+        if (fc.showOpenDialog(open) == JFileChooser.FILES_AND_DIRECTORIES) {
 
         }
         String way;

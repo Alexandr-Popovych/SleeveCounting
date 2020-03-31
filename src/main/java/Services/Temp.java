2c78372c8ad57ea05178378ff051d004839e3e0e
User <apmail2018@gmail.com>
2020-03-31 23:51:38 +0300

    new

--- src/main/java/Services/Temp.java
+++ src/main/java/Services/Temp.java
@@ -1,14 +1,23 @@
 package Services;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 
 public class Temp {
 
     public static String tempread() throws IOException {
+        try {
+            FileReader fr = new FileReader("temp.txt");
+        } catch (FileNotFoundException e) {
+            FileWriter fw = new FileWriter("temp.txt");
+            fw.write("C:\\");
+            fw.close();
+            FileReader fr = new FileReader("temp.txt");
+        }
 
-        FileReader fr = new FileReader("temp.txt");
         char[] tmp = new char[200];
         fr.read(tmp);
         String temp = new String();

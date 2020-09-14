26901c4d583bd107ef4edf562f8d2da2fe5969d6
User <a.popovych89@gmail.com>
2020-09-14 17:52:24 +0300

    Comit

--- src/main/java/Services/EReader.java
+++ src/main/java/Services/EReader.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 
 public class EReader {
-    public static void read(String way, String[] name, String designation[], double[] quantity, int first) throws IOException {
+    public static void read(String way, String[] name, String[] designation, double[] quantity, int first) throws IOException {
         FileInputStream fis = new FileInputStream(way);
         Workbook wb = new HSSFWorkbook(fis);
         for (int i = 0; i < name.length; i++) {

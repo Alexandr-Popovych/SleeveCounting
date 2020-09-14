c0ac035a854ac57e56298ca29df8598eece66181
User <a.popovych89@gmail.com>
2020-09-14 19:17:46 +0300

    Update

--- src/main/java/Services/SleeveCounting.java
+++ src/main/java/Services/SleeveCounting.java
@@ -5,9 +5,9 @@ public class SleeveCounting {
     public static void count(String[] designation, double[] quantity, String[] sleeveSize, double[] sleeveQuantity) {
 
         for (int i = 0; i < designation.length; i++) {
-            String delimeter = "-";
+            String delimiter = "-";
             String[] subStr;
-            subStr = designation[i].split(delimeter);
+            subStr = designation[i].split(delimiter);
             for (int j = 0; j < subStr.length; j++) {
                 if (sleeveSize[0].equals(subStr[j]) | (" " + sleeveSize[0] + " ").equals(subStr[j]) | (" " + sleeveSize[0] + "").equals(subStr[j]) | ("" + sleeveSize[0] + " ").equals(subStr[j])) {
                     sleeveQuantity[0] = sleeveQuantity[0] + quantity[i];
